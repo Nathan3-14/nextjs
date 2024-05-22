5e6623d30205bd2ec96617d78cdcc37a4f47a997
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-05-22 20:30:28 +0000

    Test

---
+++ cool-app/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return <>
+      <h1>Hello, Next.js!</h1>
+      <div>
+          <h2>This is a cool div</h2>
+          <p>Some text is here as well</p>
+      </div>
+  </>
+}
